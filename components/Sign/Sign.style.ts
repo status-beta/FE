29accaf392dd9d8852ede1e312ccb0b6f148b9e0
chiyoung <100222045+Returnmakdo@users.noreply.github.com>
2025-01-30 00:26:13 +0900

    SCRUM-3 sign 페이지 UI 가져오기

---
+++ components/Sign/Sign.style.ts
@@ -0,0 +1,164 @@
+import styled from 'styled-components';
+import { FlexAlignBox, FlexCenterBox, FlexColumnBox } from '@/app/shared/flex';
+
+export const Wrapper = styled.div`
+  ${FlexCenterBox};
+  width: 1500px;
+  height: 100vh;
+  margin: 0 auto;
+  background-repeat: no-repeat;
+  background-image: url('/assets/image/login.webp');
+
+  @media (max-width: 1500px) {
+    width: 100vw;
+    background-image: none;
+  }
+`;
+
+export const LoginBox = styled.div`
+  ${FlexColumnBox};
+  width: 350px;
+  align-items: center;
+`;
+export const Logo = styled.div`
+  ${FlexColumnBox};
+  align-items: center;
+`;
+
+export const LogoBox = styled.div`
+  width: 12rem;
+  cursor: pointer;
+`;
+
+export const Info = styled.div`
+  text-align: center;
+`;
+
+export const BigMent = styled.p`
+  font-weight: 700;
+  font-size: 26px;
+  margin: 2rem 0 3rem 0;
+`;
+
+export const SmallMent = styled.p`
+  margin-bottom: 1rem;
+  color: #aaaaaa;
+  font-size: 15px;
+  font-weight: 400;
+`;
+
+export const Ment = styled.div`
+  ${FlexColumnBox};
+  align-items: center;
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  font-weight: 400;
+  div {
+    ${FlexAlignBox};
+    strong {
+      font-weight: 600;
+    }
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  width: 330px;
+  ${FlexColumnBox};
+  margin-top: 3rem;
+`;
+export const LoginButton = styled.div`
+  ${FlexCenterBox};
+  width: 100%;
+  height: 2.9rem;
+  background-color: ${(props) => props.theme.color.green};
+  border-radius: 5px;
+  font-weight: 500;
+  font-size: 1.1rem;
+  color: ${(props) => props.theme.color.white};
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme.color.hoverGreen};
+  }
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  border-top: 1px solid ${(props) => props.theme.color.gray};
+  margin: 2rem 0;
+`;
+
+export const EmailLogin = styled.div`
+  position: relative;
+  ${FlexAlignBox};
+  width: 100%;
+  height: 2.9rem;
+  border: 1px solid ${(props) => props.theme.color.gray};
+  padding: 0 2rem;
+  border-radius: 1.5rem;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme.color.extraLightGray};
+  }
+  svg {
+    color: ${(props) => props.theme.color.gray};
+    font-size: 1.2rem;
+  }
+  span {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    font-weight: 500;
+    font-size: 1.1rem;
+    text-align: center;
+  }
+`;
+
+export const EasyJoin = styled.span`
+  ${FlexCenterBox};
+  color: rgba(0, 0, 0, 0.6);
+  margin: 2rem 0 1.5rem 0;
+`;
+
+export const SocialList = styled.ul`
+  width: 100%;
+  ${FlexColumnBox};
+`;
+
+export const SocialItem = styled(EmailLogin)`
+  color: white;
+  border: none;
+  box-shadow: 0px 2px 3px ${(props) => props.theme.color.lightGray};
+  margin-bottom: 0.6rem;
+  .naver {
+    height: 30px;
+    border-radius: 50%;
+    outline: 2.5px solid #dcffc7;
+  }
+  .kakao {
+    height: 30px;
+    border-radius: 50%;
+    outline: 2.5px solid #fff9c4;
+  }
+  .google {
+    height: 30px;
+    border-radius: 50%;
+    outline: 2.5px solid #ffffff;
+    outline-offset: -2px;
+  }
+  img {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+  }
+  &:first-child {
+    background-color: #02c75a;
+  }
+  &:nth-child(2) {
+    color: inherit;
+    background-color: #ffe80f;
+  }
+  &:last-child {
+    color: #ffffff;
+    background-color: #5383ec;
+  }
+`;
---
+++ components/Sign/Sign.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import Link from 'next/link';
+import BigLogoSvg from '@/public/assets/svg/BigLogoSvg';
+import {
+  LoginBox,
+  LoginButton,
+  EmailLogin,
+  LogoBox,
+  Info,
+  BigMent,
+  SmallMent,
+  Ment,
+  EasyJoin,
+  Logo,
+  Wrapper,
+  ButtonContainer,
+  Line,
+  SocialItem,
+  SocialList,
+} from './Sign.style';
+import Image from 'next/image';
+
+const Sign = () => {
+  return (
+    <Wrapper>
+      <LoginBox>
+        <Logo>
+          <Link href="/">
+            <LogoBox>
+              <BigLogoSvg />
+            </LogoBox>
+          </Link>
+          <Info>
+            <BigMent>나의 팀을 이해하는 곳</BigMent>
+            <Ment>
+              <SmallMent>일을 잘하는 것만큼 내 팀의 상태를 파악하는 것도 중요하죠.</SmallMent>
+              <SmallMent>스테이터스로 팀원의 일정과 상태를 체크하세요.</SmallMent>
+            </Ment>
+          </Info>
+        </Logo>
+        <ButtonContainer>
+          <Link href="/login">
+            <LoginButton>로그인하기</LoginButton>
+          </Link>
+          <Line />
+          <Link href="/signup">
+            <EmailLogin>
+              <FontAwesomeIcon icon={faEnvelope} />
+              <span>이메일로 시작하기</span>
+            </EmailLogin>
+          </Link>
+          <EasyJoin>간편하게 가입하세요!</EasyJoin>
+          <SocialList>
+            <SocialItem>
+              <div className="naver">
+                <Image src={'/assets/image/btnG_아이콘원형.png'} alt="NaverLogin" width={32} height={32} />
+              </div>
+              <span>네이버 로그인</span>
+            </SocialItem>
+            <SocialItem>
+              <div className="kakao">
+                <Image src={'/assets/image/kakaotalk-icon.png'} alt="kakaoLogin" width={32} height={32} />
+              </div>
+              <span>카카오톡 로그인</span>
+            </SocialItem>
+            <SocialItem>
+              <div className="google">
+                <Image src={'/assets/image/icons8-구글-로고-48.png'} alt="GoogleLogin" width={32} height={32} />
+              </div>
+              <span>구글 로그인</span>
+            </SocialItem>
+          </SocialList>
+        </ButtonContainer>
+      </LoginBox>
+    </Wrapper>
+  );
+};
+
+export default Sign;
