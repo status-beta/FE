c88986b6efb9880438440df18859f8a3750f950b
minyoung <zhal7779@naver.com>
2025-02-02 16:26:24 +0900

    SCRUM-3 Feat:  comment 타입 정의

---
+++ types/commentType.ts
@@ -0,0 +1,10 @@
+export interface CommentType {
+  commentId: number;
+  comment: string;
+  groupAvatarImg: string;
+  groupUserNickname: string;
+  groupUserId: number;
+  createdAt: string;
+  commentLike: boolean;
+  likeCount: number;
+}
